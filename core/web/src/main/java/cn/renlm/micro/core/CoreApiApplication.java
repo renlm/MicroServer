686009e3d127737357257ad84deaadeb2e7b004b
renlm <renlm@21cn.com>
2024-12-21 22:32:59 +0800

    up

--- core/web/src/main/java/cn/renlm/micro/core/CoreApiApplication.java
+++ core/web/src/main/java/cn/renlm/micro/core/CoreApiApplication.java
@@ -1,14 +1,4 @@
-package cn.renlm.micro;
-
-import static org.springframework.security.config.Customizer.withDefaults;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+package cn.renlm.micro.core;
 
 /**
  * 配置中心
@@ -23,16 +13,6 @@ public class CoreApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigApplication.class, args);
-	}
-
-	@Bean
-	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		String[] anonymousRequests = { "/monitor", "/actuator/**" };
-		http.authorizeHttpRequests(r -> r.requestMatchers(anonymousRequests).permitAll().anyRequest().authenticated());
-		http.formLogin(withDefaults());
-		http.httpBasic(withDefaults());
-		http.csrf(csrf -> csrf.disable());
-		return http.build();
 	}
 
 }
