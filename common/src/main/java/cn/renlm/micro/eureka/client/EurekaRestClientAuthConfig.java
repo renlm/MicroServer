ff116addd9c3b5b250fa091c4acefc741ff64696
renlm-a <renlm-a@glodon.com>
2024-12-21 16:41:52 +0800

    up

--- common/src/main/java/cn/renlm/micro/eureka/client/EurekaRestClientAuthConfig.java
+++ common/src/main/java/cn/renlm/micro/eureka/client/EurekaRestClientAuthConfig.java
@@ -12,7 +12,6 @@ import java.security.SecureRandom;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.configuration.SSLContextFactory;
 import org.springframework.cloud.configuration.TlsProperties;
@@ -32,7 +31,6 @@ import cn.renlm.micro.util.CsrfUtil;
  * @author RenLiMing(任黎明)
  *
  */
-@ConditionalOnProperty(value = "eureka.client.restclient.enabled", havingValue = "true")
 @ConditionalOnClass({ RestClient.class })
 @ConditionalOnMissingBean({ EurekaJersey3ClientAuthConfig.class })
 @EnableConfigurationProperties({ EurekaAuthProperties.class })
