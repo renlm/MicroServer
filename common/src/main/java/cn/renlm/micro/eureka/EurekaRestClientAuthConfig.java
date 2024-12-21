57b19fac6ba3d7bcfdc0e77a9d872760511e2b0c
renlm-a <renlm-a@glodon.com>
2024-12-21 15:11:58 +0800

    up

--- common/src/main/java/cn/renlm/micro/eureka/EurekaRestClientAuthConfig.java
+++ common/src/main/java/cn/renlm/micro/eureka/EurekaRestClientAuthConfig.java
@@ -5,13 +5,19 @@ import static cn.renlm.micro.eureka.EurekaServerAuthConfig.SIGN_HEADER_TIMESTAMP
 import static cn.renlm.micro.eureka.EurekaServerAuthConfig.X_SERVER_TOKEN;
 import static cn.renlm.micro.eureka.EurekaServerAuthConfig.X_XSRF_TOKEN;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.security.SecureRandom;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.configuration.SSLContextFactory;
+import org.springframework.cloud.configuration.TlsProperties;
 import org.springframework.cloud.netflix.eureka.http.EurekaClientHttpRequestFactorySupplier;
 import org.springframework.cloud.netflix.eureka.http.RestClientDiscoveryClientOptionalArgs;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.client.RestClient;
 
@@ -24,16 +30,18 @@ import cn.renlm.micro.util.CsrfUtil;
  * @author RenLiMing(任黎明)
  *
  */
-@ConditionalOnClass(RestClient.class)
+@ConditionalOnBean({ RestClientDiscoveryClientOptionalArgs.class })
 @EnableConfigurationProperties({ EurekaAuthProperties.class })
 public class EurekaRestClientAuthConfig {
 
 	private SecureRandom secureRandom = new SecureRandom();
 
 	@Bean
+	@Primary
 	public RestClientDiscoveryClientOptionalArgs restClientDiscoveryClientOptionalArgs(EurekaAuthProperties env,
-			EurekaClientHttpRequestFactorySupplier eurekaClientHttpRequestFactorySupplier) {
-		RestClientDiscoveryClientOptionalArgs discoveryClientOptionalArgs = new RestClientDiscoveryClientOptionalArgs(
+			TlsProperties tlsProperties, EurekaClientHttpRequestFactorySupplier eurekaClientHttpRequestFactorySupplier,
+			ObjectProvider<RestClient.Builder> restClientBuilderProvider) throws GeneralSecurityException, IOException {
+		RestClientDiscoveryClientOptionalArgs args = new RestClientDiscoveryClientOptionalArgs(
 				eurekaClientHttpRequestFactorySupplier, () -> {
 					return RestClient.builder().requestInterceptor(((request, body, execution) -> {
 						String serverToken = CsrfUtil.createServerToken();
@@ -48,8 +56,12 @@ public class EurekaRestClientAuthConfig {
 						return execution.execute(request, body);
 					}));
 				});
+		if (tlsProperties.isEnabled()) {
+			SSLContextFactory factory = new SSLContextFactory(tlsProperties);
+			args.setSSLContext(factory.createSSLContext());
+		}
 		{
-			return discoveryClientOptionalArgs;
+			return args;
 		}
 	}
 
--- common/src/main/java/cn/renlm/micro/eureka/EurekaWebClientAuthConfig.java
+++ common/src/main/java/cn/renlm/micro/eureka/EurekaWebClientAuthConfig.java
@@ -5,13 +5,17 @@ import static cn.renlm.micro.eureka.EurekaServerAuthConfig.SIGN_HEADER_TIMESTAMP
 import static cn.renlm.micro.eureka.EurekaServerAuthConfig.X_SERVER_TOKEN;
 import static cn.renlm.micro.eureka.EurekaServerAuthConfig.X_XSRF_TOKEN;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.security.SecureRandom;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.netflix.eureka.http.EurekaClientHttpRequestFactorySupplier;
+import org.springframework.cloud.configuration.SSLContextFactory;
+import org.springframework.cloud.configuration.TlsProperties;
 import org.springframework.cloud.netflix.eureka.http.WebClientDiscoveryClientOptionalArgs;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -24,32 +28,36 @@ import cn.renlm.micro.util.CsrfUtil;
  * @author RenLiMing(任黎明)
  *
  */
-@ConditionalOnClass(WebClient.class)
+@ConditionalOnBean({ WebClientDiscoveryClientOptionalArgs.class })
 @EnableConfigurationProperties({ EurekaAuthProperties.class })
 public class EurekaWebClientAuthConfig {
 
 	private SecureRandom secureRandom = new SecureRandom();
 
 	@Bean
+	@Primary
 	public WebClientDiscoveryClientOptionalArgs webClientDiscoveryClientOptionalArgs(EurekaAuthProperties env,
-			EurekaClientHttpRequestFactorySupplier eurekaClientHttpRequestFactorySupplier) {
-		WebClientDiscoveryClientOptionalArgs discoveryClientOptionalArgs = new WebClientDiscoveryClientOptionalArgs(
-				() -> {
-					return WebClient.builder().filter((request, next) -> {
-						String serverToken = CsrfUtil.createServerToken();
-						String csrfToken = CsrfUtil.createCsrfToken(secureRandom, serverToken);
-						String timestamp = String.valueOf(System.currentTimeMillis());
-						String secretKey = env.getSecretKey();
-						String sign = DigestUtils.md5DigestAsHex((csrfToken + timestamp + secretKey).getBytes());
-						request.headers().add(X_SERVER_TOKEN, serverToken);
-						request.headers().add(X_XSRF_TOKEN, csrfToken);
-						request.headers().add(SIGN_HEADER_TIMESTAMP, timestamp);
-						request.headers().add(SIGN_HEADER_SIGN, sign);
-						return next.exchange(request);
-					});
-				});
+			TlsProperties tlsProperties) throws GeneralSecurityException, IOException {
+		WebClientDiscoveryClientOptionalArgs args = new WebClientDiscoveryClientOptionalArgs(() -> {
+			return WebClient.builder().filter((request, next) -> {
+				String serverToken = CsrfUtil.createServerToken();
+				String csrfToken = CsrfUtil.createCsrfToken(secureRandom, serverToken);
+				String timestamp = String.valueOf(System.currentTimeMillis());
+				String secretKey = env.getSecretKey();
+				String sign = DigestUtils.md5DigestAsHex((csrfToken + timestamp + secretKey).getBytes());
+				request.headers().add(X_SERVER_TOKEN, serverToken);
+				request.headers().add(X_XSRF_TOKEN, csrfToken);
+				request.headers().add(SIGN_HEADER_TIMESTAMP, timestamp);
+				request.headers().add(SIGN_HEADER_SIGN, sign);
+				return next.exchange(request);
+			});
+		});
+		if (tlsProperties.isEnabled()) {
+			SSLContextFactory factory = new SSLContextFactory(tlsProperties);
+			args.setSSLContext(factory.createSSLContext());
+		}
 		{
-			return discoveryClientOptionalArgs;
+			return args;
 		}
 	}
 
