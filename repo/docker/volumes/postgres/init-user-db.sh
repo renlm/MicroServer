fe27d985087c69a0edd0fc945407e5c11c2986b6
renlm-a <renlm-a@glodon.com>
2024-12-22 18:20:53 +0800

    up

--- repo/docker/volumes/postgres/init-user-db.sh
+++ repo/docker/volumes/postgres/init-user-db.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e
 
-cat <<EOF | tee /docker-entrypoint-initdb.d/.pgpass
+export PGPASSFILE=/docker-entrypoint-initdb.d/.pgpass
+cat <<EOF | tee $PGPASSFILE
 localhost:5432:postgres:postgres:$POSTGRES_PASSWORD
 EOF
-export PGPASSFILE=/docker-entrypoint-initdb.d/.pgpass
+
 psql -v ON_ERROR_STOP=1 <<-EOSQL
-	CREATE USER share WITH PASSWORD '$POSTGRES_PASSWORD';
-	CREATE DATABASE share OWNER share;
-	GRANT ALL PRIVILEGES ON DATABASE share TO share;
+	CREATE USER dev WITH PASSWORD '$POSTGRES_PASSWORD';
+	CREATE DATABASE dev OWNER dev;
+	GRANT ALL PRIVILEGES ON DATABASE dev TO dev;
 EOSQL
