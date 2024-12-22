d66261d84583d94ed14c4f1586356b26a3673963
renlm-a <renlm-a@glodon.com>
2024-12-22 19:50:03 +0800

    up

---
+++ repo/docker/volumes/clickhouse/init-db.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+clickhouse client -n <<-EOSQL
+    CREATE DATABASE logging;
+    CREATE USER IF NOT EXISTS 'logging' IDENTIFIED BY 'KK_fyw4bnmdf4Ksp7IMYuPWD@20stdt^tn50jlsfy';
+    GRANT ALL ON logging.* TO 'logging';
+EOSQL
