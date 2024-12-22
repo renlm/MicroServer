737aa29a37adff789112cafb7564abac4bd11ca4
renlm-a <renlm-a@glodon.com>
2024-12-22 18:17:43 +0800

    up

--- repo/docker/volumes/postgres/init-user-db.sh
+++ repo/docker/volumes/postgres/init-user-db.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+cat <<EOF | tee /docker-entrypoint-initdb.d/.pgpass
+localhost:5432:postgres:postgres:$POSTGRES_PASSWORD
+EOF
 export PGPASSFILE=/docker-entrypoint-initdb.d/.pgpass
 psql -v ON_ERROR_STOP=1 <<-EOSQL
 	CREATE USER share WITH PASSWORD '$POSTGRES_PASSWORD';
