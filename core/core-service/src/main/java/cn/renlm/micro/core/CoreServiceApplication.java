c4259fd428f473b89b2eea1b51c8bf9689dc98da
renlm-a <renlm-a@glodon.com>
2024-12-22 17:25:25 +0800

    up

--- core/core-service/src/main/java/cn/renlm/micro/core/CoreServiceApplication.java
+++ core/core-service/src/main/java/cn/renlm/micro/core/CoreServiceApplication.java
@@ -2,6 +2,7 @@ package cn.renlm.micro.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * CoreService
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author RenLiMing(任黎明)
  *
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CoreServiceApplication {
 
