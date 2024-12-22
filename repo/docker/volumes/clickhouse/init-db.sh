8273d862292f974fccf28812c1a934a26ad22d82
renlm-a <renlm-a@glodon.com>
2024-12-22 20:11:46 +0800

    up

--- repo/docker/volumes/clickhouse/init-db.sh
+++ repo/docker/volumes/clickhouse/init-db.sh
@@ -6,5 +6,10 @@ clickhouse client -n <<-EOSQL
     CREATE USER IF NOT EXISTS 'logging' IDENTIFIED BY 'KK_fyw4bnmdf4Ksp7IMYuPWD@20stdt^tn50jlsfy';
     GRANT ALL ON logging.* TO 'logging';
     use logging;
-    
+    CREATE NAMED COLLECTION local_mysql_dev AS
+        host = 'mysql',
+        port = 3306,
+        database = 'dev',
+        user = 'dev',
+        password = 'KK_fyw4bnmdf4Ksp7IMYuPWD@20stdt^tn50jlsfy';
 EOSQL
