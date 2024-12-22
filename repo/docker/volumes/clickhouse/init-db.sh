b70d1a77f8c9dded0958ff700689314fa78b6c17
renlm-a <renlm-a@glodon.com>
2024-12-22 20:27:47 +0800

    up

--- repo/docker/volumes/clickhouse/init-db.sh
+++ repo/docker/volumes/clickhouse/init-db.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 
+# https://clickhouse.com/docs/en/engines/table-engines
 clickhouse client -n <<-EOSQL
     CREATE DATABASE logging;
-    CREATE USER IF NOT EXISTS 'logging' IDENTIFIED BY '$CLICKHOUSE_PASSWORD';
-    GRANT ALL ON logging.* TO 'logging';
+    CREATE USER logging IDENTIFIED BY '$CLICKHOUSE_PASSWORD';
+    GRANT ALL ON logging.* TO logging;
     use dev;
     CREATE NAMED COLLECTION local_mysql_dev AS host = 'mysql', port = 3306, database = 'dev', user = 'dev', password = '$CLICKHOUSE_PASSWORD';
     CREATE NAMED COLLECTION local_postgres_dev AS host = 'postgres', port = 5432, database = 'dev', schema = 'public', user = 'dev', password = '$CLICKHOUSE_PASSWORD';
