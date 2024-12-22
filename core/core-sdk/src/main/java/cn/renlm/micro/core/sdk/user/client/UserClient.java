ec9875648e187d000440b6b4c50161664f6f4a93
renlm-a <renlm-a@glodon.com>
2024-12-22 15:56:29 +0800

    up

--- core/core-sdk/src/main/java/cn/renlm/micro/core/sdk/user/client/UserClient.java
+++ core/core-sdk/src/main/java/cn/renlm/micro/core/sdk/user/client/UserClient.java
@@ -1,7 +1,11 @@
 package cn.renlm.micro.core.sdk.user.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
 import static cn.renlm.micro.constant.ServiceNameConstants.CORE_SERVICE;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import cn.renlm.micro.core.sdk.user.dto.UserDetails;
 
 /**
  * 用户
@@ -9,7 +13,16 @@ import static cn.renlm.micro.constant.ServiceNameConstants.CORE_SERVICE;
  * @author RenLiMing(任黎明)
  *
  */
-@FeignClient(contextId = "userClient", name = CORE_SERVICE)
-public class UserClient {
+@FeignClient(contextId = "userClient", path = "/user", name = CORE_SERVICE)
+public interface UserClient {
+
+	/**
+	 * 根据登录账号获取用户信息
+	 * 
+	 * @param userName
+	 * @return
+	 */
+	@GetMapping(value = "/loadUserByUserName")
+	UserDetails loadUserByUserName(String userName);
 
 }
