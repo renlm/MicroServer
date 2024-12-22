d68b376eb239bb20ca69da9d37f1d995b0ea47ed
renlm-a <renlm-a@glodon.com>
2024-12-22 18:45:07 +0800

    up

--- repo/docker/volumes/mysql/init-user-db.sql
+++ repo/docker/volumes/mysql/init-user-db.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE dev;
-CREATE USER 'dev'@'%' IDENTIFIED WITH caching_sha2_password BY 'KK_fyw4bnmdf4Ksp7IMYuPWD@20stdt^tn50jlsfy';
-GRANT ALL PRIVILEGES ON dev.* to 'dev'@'%';
+CREATE DATABASE mygraph;
+CREATE USER 'mygraph'@'%' IDENTIFIED WITH caching_sha2_password BY 'KK_fyw4bnmdf4Ksp7IMYuPWD@20stdt^tn50jlsfy';
+GRANT ALL PRIVILEGES ON mygraph.* to 'mygraph'@'%';
 FLUSH PRIVILEGES;
