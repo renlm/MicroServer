b8e90e8c5437a4c8c317de674327361ff05034a1
renlm <renlm@21cn.com>
2024-12-28 21:55:33 +0800

    up

---
+++ core/core-web/src/main/java/cn/renlm/micro/core/config/WebConfig.java
@@ -0,0 +1,56 @@
+package cn.renlm.micro.core.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.ContentVersionStrategy;
+import org.springframework.web.servlet.resource.LiteWebJarsResourceResolver;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+import org.springframework.web.servlet.resource.VersionResourceResolver;
+
+/**
+ * Web 配置
+ *
+ * @author RenLiMing(任黎明)
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Autowired
+	private ThymeleafProperties thymeleafProperties;
+
+	@Override
+	public void addFormatters(FormatterRegistry registry) {
+		registry.addConverter(new Converter<String, String>() {
+			@Override
+			public String convert(String source) {
+				return StringUtils.hasText(source) ? source.stripLeading().stripTrailing() : source;
+			}
+		});
+	}
+
+	@Override
+	// @formatter:off
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		VersionResourceResolver resolver = new VersionResourceResolver();
+		resolver.addVersionStrategy(new ContentVersionStrategy(), "/**");
+
+		registry.addResourceHandler("/static/**")
+			.addResourceLocations("classpath:/static/")
+			.resourceChain(thymeleafProperties.isCache())
+			.addResolver(resolver);
+
+		registry.addResourceHandler("/webjars/**")
+			.addResourceLocations("classpath:/META-INF/resources/webjars/")
+			.resourceChain(thymeleafProperties.isCache())
+			.addResolver(resolver)
+			.addResolver(new LiteWebJarsResourceResolver())
+			.addResolver(new PathResourceResolver());
+	}
+	// @formatter:on
+}
