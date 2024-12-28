b68255e081f6f8cc09f79dfa724a1d4017dc75aa
renlm-a <renlm-a@glodon.com>
2024-12-28 15:44:47 +0800

    用户

--- core/core-service/src/main/resources/db/schema/rbac.sql
+++ core/core-service/src/main/resources/db/schema/rbac.sql
@@ -1,3 +1,4 @@
+-- 用户
 INSERT INTO "user"("user_id", "username", "password", "nickname", "realname", "birthday", "sex", "mobile", "email")
 VALUES ('B89E08CA5C0D4C26AAEE66AD7DD05F32', 'default', '123654', '管理员', '令狐冲', NOW(), 'M', '17338158607', 'renlm@21cn.com')
 ;
