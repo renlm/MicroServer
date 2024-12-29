cd9efa0ef421aa1c5a7007a7142399aea692daff
renlm-a <renlm-a@glodon.com>
2024-12-29 16:17:13 +0800

    up

--- core/core-web/src/main/java/cn/renlm/micro/core/controller/LoginController.java
+++ core/core-web/src/main/java/cn/renlm/micro/core/controller/LoginController.java
@@ -1,10 +1,14 @@
 package cn.renlm.micro.core.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import cn.renlm.micro.core.dto.UserClaim;
+import cn.renlm.micro.core.dto.UserDetails;
 import cn.renlm.micro.core.util.SessionUtil;
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -30,6 +34,19 @@ public class LoginController {
 		String aesKey = SessionUtil.getAesKey(request);
 		model.put(SessionUtil.AES_KEY, aesKey);
 		return "login";
+	}
+
+	/**
+	 * 获取当前登录用户信息
+	 * 
+	 * @param authentication
+	 * @return
+	 */
+	@ResponseBody
+	@GetMapping("/getCurrentUser")
+	public UserClaim getCurrentUser(Authentication authentication) {
+		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+		return userDetails.toClaim();
 	}
 
 }
