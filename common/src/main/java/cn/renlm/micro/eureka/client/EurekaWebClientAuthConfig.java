b9d78a34a7bfbd403f00e3f0f4ea48aedab48c52
renlm-a <renlm-a@glodon.com>
2024-12-29 19:00:50 +0800

    up

--- common/src/main/java/cn/renlm/micro/eureka/client/EurekaWebClientAuthConfig.java
+++ common/src/main/java/cn/renlm/micro/eureka/client/EurekaWebClientAuthConfig.java
@@ -5,13 +5,20 @@ import static cn.renlm.micro.eureka.EurekaServerAuthConfig.SIGN_HEADER_TIMESTAMP
 import static cn.renlm.micro.eureka.EurekaServerAuthConfig.X_SERVER_TOKEN;
 import static cn.renlm.micro.eureka.EurekaServerAuthConfig.X_XSRF_TOKEN;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.security.SecureRandom;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.configuration.SSLContextFactory;
+import org.springframework.cloud.configuration.TlsProperties;
+import org.springframework.cloud.netflix.eureka.http.WebClientDiscoveryClientOptionalArgs;
+import org.springframework.cloud.netflix.eureka.http.WebClientTransportClientFactories;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.reactive.function.client.ClientRequest;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -33,7 +40,6 @@ public class EurekaWebClientAuthConfig {
 
 	private SecureRandom secureRandom = new SecureRandom();
 
-	@Bean
 	public WebClient.Builder eurekaWebClientBuilder(EurekaAuthProperties env) {
 		return WebClient.builder().filter((req, next) -> {
 			final String serverToken = CsrfUtil.createServerToken();
@@ -47,6 +53,30 @@ public class EurekaWebClientAuthConfig {
 			request.header(SIGN_HEADER_TIMESTAMP, timestamp);
 			request.header(SIGN_HEADER_SIGN, sign);
 			return next.exchange(request.build());
+		});
+	}
+
+	@Bean
+	@Primary
+	public WebClientDiscoveryClientOptionalArgs webClientDiscoveryClientOptionalArgs(EurekaAuthProperties env,
+			TlsProperties tlsProperties) throws GeneralSecurityException, IOException {
+		WebClientDiscoveryClientOptionalArgs args = new WebClientDiscoveryClientOptionalArgs(() -> {
+			return this.eurekaWebClientBuilder(env);
+		});
+		if (tlsProperties.isEnabled()) {
+			SSLContextFactory factory = new SSLContextFactory(tlsProperties);
+			args.setSSLContext(factory.createSSLContext());
+		}
+		{
+			return args;
+		}
+	}
+
+	@Bean
+	@Primary
+	public WebClientTransportClientFactories webClientTransportClientFactories(EurekaAuthProperties env) {
+		return new WebClientTransportClientFactories(() -> {
+			return this.eurekaWebClientBuilder(env);
 		});
 	}
 
