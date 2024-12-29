012890e56f18dd6a85108c6c2f0d7fe7e83e9e9c
renlm-a <renlm-a@glodon.com>
2024-12-29 18:39:45 +0800

    up

--- common/src/main/java/cn/renlm/micro/eureka/client/EurekaWebClientAuthConfig.java
+++ common/src/main/java/cn/renlm/micro/eureka/client/EurekaWebClientAuthConfig.java
@@ -16,6 +16,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.configuration.SSLContextFactory;
 import org.springframework.cloud.configuration.TlsProperties;
 import org.springframework.cloud.netflix.eureka.http.WebClientDiscoveryClientOptionalArgs;
+import org.springframework.cloud.netflix.eureka.http.WebClientTransportClientFactories;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.util.DigestUtils;
@@ -39,22 +40,27 @@ public class EurekaWebClientAuthConfig {
 	private SecureRandom secureRandom = new SecureRandom();
 
 	@Bean
+	public WebClient.Builder eurekaWebClientBuilder(EurekaAuthProperties env) {
+		return WebClient.builder().filter((request, next) -> {
+			String serverToken = CsrfUtil.createServerToken();
+			String csrfToken = CsrfUtil.createCsrfToken(secureRandom, serverToken);
+			String timestamp = String.valueOf(System.currentTimeMillis());
+			String secretKey = env.getSecretKey();
+			String sign = DigestUtils.md5DigestAsHex((csrfToken + timestamp + secretKey).getBytes());
+			request.headers().add(X_SERVER_TOKEN, serverToken);
+			request.headers().add(X_XSRF_TOKEN, csrfToken);
+			request.headers().add(SIGN_HEADER_TIMESTAMP, timestamp);
+			request.headers().add(SIGN_HEADER_SIGN, sign);
+			return next.exchange(request);
+		});
+	}
+
+	@Bean
 	@Primary
 	public WebClientDiscoveryClientOptionalArgs webClientDiscoveryClientOptionalArgs(EurekaAuthProperties env,
 			TlsProperties tlsProperties) throws GeneralSecurityException, IOException {
 		WebClientDiscoveryClientOptionalArgs args = new WebClientDiscoveryClientOptionalArgs(() -> {
-			return WebClient.builder().filter((request, next) -> {
-				String serverToken = CsrfUtil.createServerToken();
-				String csrfToken = CsrfUtil.createCsrfToken(secureRandom, serverToken);
-				String timestamp = String.valueOf(System.currentTimeMillis());
-				String secretKey = env.getSecretKey();
-				String sign = DigestUtils.md5DigestAsHex((csrfToken + timestamp + secretKey).getBytes());
-				request.headers().add(X_SERVER_TOKEN, serverToken);
-				request.headers().add(X_XSRF_TOKEN, csrfToken);
-				request.headers().add(SIGN_HEADER_TIMESTAMP, timestamp);
-				request.headers().add(SIGN_HEADER_SIGN, sign);
-				return next.exchange(request);
-			});
+			return this.eurekaWebClientBuilder(env);
 		});
 		if (tlsProperties.isEnabled()) {
 			SSLContextFactory factory = new SSLContextFactory(tlsProperties);
@@ -63,6 +69,13 @@ public class EurekaWebClientAuthConfig {
 		{
 			return args;
 		}
+	}
+
+	@Bean
+	public WebClientTransportClientFactories webClientTransportClientFactories(EurekaAuthProperties env) {
+		return new WebClientTransportClientFactories(() -> {
+			return this.eurekaWebClientBuilder(env);
+		});
 	}
 
 }
