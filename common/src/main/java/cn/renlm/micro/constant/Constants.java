cc19e9b22793fb84a95ba6558c44e05deb47c0f9
renlm-a <renlm-a@glodon.com>
2024-12-30 16:50:10 +0800

    up

---
+++ common/src/main/java/cn/renlm/micro/constant/Constants.java
@@ -0,0 +1,21 @@
+package cn.renlm.micro.constant;
+
+/**
+ * 公共常量
+ * 
+ * @author RenLiMing(任黎明)
+ *
+ */
+public interface Constants {
+
+	/**
+	 * 请求头负载均衡标记名称
+	 */
+	public static final String HINT_HEADER_NAME = "x-lb-hint";
+
+	/**
+	 * 服务元数据负载均衡标记名称
+	 */
+	public static final String HINT_METADATA_NAME = "hint";
+
+}
---
+++ gateway/src/main/java/cn/renlm/micro/filter/AddHintHeaderGatewayFilter.java
@@ -0,0 +1,54 @@
+package cn.renlm.micro.filter;
+
+import static cn.renlm.micro.constant.Constants.HINT_HEADER_NAME;
+
+import java.util.Objects;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.server.ServerWebExchange;
+
+import cn.renlm.micro.core.model.rbac.UserClaim;
+import cn.renlm.micro.core.sdk.rbac.SessionClient;
+import jakarta.annotation.Resource;
+import reactor.core.publisher.Mono;
+
+/**
+ * 添加负载标记
+ * 
+ * @author RenLiMing(任黎明)
+ *
+ */
+@Component
+public class AddHintHeaderGatewayFilter implements GlobalFilter, Ordered {
+
+	@Resource
+	private SessionClient sessionClient;
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		ServerHttpRequest request = exchange.getRequest();
+		String hint = request.getHeaders().getFirst(HINT_HEADER_NAME);
+		if (!StringUtils.hasText(hint)) {
+			UserClaim userClaim = sessionClient.getCurrentUser();
+			if (Objects.nonNull(userClaim)) {
+				// @formatter:off
+				exchange = exchange.mutate().request(req -> req.headers(headers -> headers.set(HINT_HEADER_NAME, userClaim.getHint())).build()).build();
+				// @formatter:on
+			}
+		}
+		{
+			return chain.filter(exchange);
+		}
+	}
+
+	@Override
+	public int getOrder() {
+		return Ordered.HIGHEST_PRECEDENCE;
+	}
+
+}
