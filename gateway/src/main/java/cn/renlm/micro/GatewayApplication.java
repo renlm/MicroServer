0e705f6e65f86ec7cf47dcb285250395a714a902
renlm-a <renlm-a@glodon.com>
2024-12-31 10:46:44 +0800

    up

--- gateway/src/main/java/cn/renlm/micro/GatewayApplication.java
+++ gateway/src/main/java/cn/renlm/micro/GatewayApplication.java
@@ -1,9 +1,16 @@
 package cn.renlm.micro;
 
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
 
 /**
  * 服务网关
@@ -18,6 +25,12 @@ public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	@ConditionalOnMissingBean
+	public HttpMessageConverters messageConverters(ObjectProvider<HttpMessageConverter<?>> converters) {
+		return new HttpMessageConverters(converters.orderedStream().collect(Collectors.toList()));
 	}
 
 }
--- gateway/src/main/java/cn/renlm/micro/filter/AddHintHeaderGatewayFilter.java
+++ gateway/src/main/java/cn/renlm/micro/filter/AddHintHeaderGatewayFilter.java
@@ -3,6 +3,7 @@ package cn.renlm.micro.filter;
 import static cn.renlm.micro.constant.Constants.HINT_HEADER_NAME;
 
 import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
 
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
@@ -11,11 +12,14 @@ import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.server.ServerWebExchange;
 
 import cn.renlm.micro.core.model.rbac.UserClaim;
 import cn.renlm.micro.core.sdk.rbac.SessionClient;
 import jakarta.annotation.Resource;
+import lombok.SneakyThrows;
 import reactor.core.publisher.Mono;
 
 /**
@@ -32,11 +36,16 @@ public class AddHintHeaderGatewayFilter implements GlobalFilter, Ordered {
 	private SessionClient sessionClient;
 
 	@Override
+	@SneakyThrows
 	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
 		ServerHttpRequest request = exchange.getRequest();
 		String hint = request.getHeaders().getFirst(HINT_HEADER_NAME);
 		if (!StringUtils.hasText(hint)) {
-			UserClaim userClaim = sessionClient.getCurrentUser();
+			RequestAttributes attributes = RequestContextHolder.getRequestAttributes();
+			UserClaim userClaim = CompletableFuture.supplyAsync(() -> {
+				RequestContextHolder.setRequestAttributes(attributes);
+				return sessionClient.getCurrentUser();
+			}).get();
 			if (Objects.nonNull(userClaim)) {
 				// @formatter:off
 				exchange = exchange.mutate().request(req -> req.headers(headers -> headers.set(HINT_HEADER_NAME, userClaim.getHint())).build()).build();
