13c015bb5b19e13ccc7888b88603f5696477cc11
renlm-a <renlm-a@glodon.com>
2025-01-03 22:53:02 +0800

    up

--- core/core-web/start-up.sh
+++ core/core-web/start-up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-INSTANCE_HOSTNAME=${POD_IP//./-}.${SERVICE_NAME}.${POD_NAMESPACE}.svc.cluster.local
-export INSTANCE_HOSTNAME=$INSTANCE_HOSTNAME
+INSTANCE_NAME=${POD_IP//./-}.${SERVICE_NAME}.${POD_NAMESPACE}.svc.cluster.local
+export HOSTNAME=$INSTANCE_NAME
 
 java -Djava.security.egd=file:/dev/./urandom -jar /app.jar
