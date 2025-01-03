cd5080624d43e9ffcdfb5d7dbdbdf944666f892e
renlm <renlm@21cn.com>
2025-01-04 01:24:31 +0800

    up

--- core/core-web/start-up.sh
+++ core/core-web/start-up.sh
@@ -2,8 +2,8 @@
 set -e
 
 POD_FULLNAME=${POD_IP//./-}.${POD_SERVICE_NAME}.${POD_NAMESPACE}.svc.cluster.local
-sed -i '$a'"export POD_FULLNAME=$POD_FULLNAME"'' ~/.bashrc
 echo "$(sed -i '$a'"$POD_IP $POD_FULLNAME"'' /etc/hosts)" > /etc/hosts
+sed -i '$a'"export POD_FULLNAME=$POD_FULLNAME"'' ~/.bashrc
 
 source ~/.bashrc
 java -Djava.security.egd=file:/dev/./urandom -jar /app.jar
