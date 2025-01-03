8aef3c0ca2908b076e9f29d35b9b5a0f77c8f4fe
renlm <renlm@21cn.com>
2025-01-04 01:27:10 +0800

    up

--- core/core-web/start-up.sh
+++ core/core-web/start-up.sh
@@ -2,7 +2,7 @@
 set -e
 
 POD_FULLNAME=${POD_IP//./-}.${POD_SERVICE_NAME}.${POD_NAMESPACE}.svc.cluster.local
-echo "$(sed -i '$a'"$POD_IP $POD_FULLNAME"'' /etc/hosts)" > /etc/hosts
+echo "$(sed '$a'"$POD_IP $POD_FULLNAME"'' /etc/hosts)" > /etc/hosts
 sed -i '$a'"export POD_FULLNAME=$POD_FULLNAME"'' ~/.bashrc
 
 source ~/.bashrc
