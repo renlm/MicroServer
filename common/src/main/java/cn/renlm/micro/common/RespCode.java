e4690b2c92aac47fb34ec22a6b4be1f629acf1bb
renlm-a <renlm-a@glodon.com>
2025-01-04 17:54:56 +0800

    up

--- common/src/main/java/cn/renlm/micro/common/RespCode.java
+++ common/src/main/java/cn/renlm/micro/common/RespCode.java
@@ -1,5 +1,7 @@
 package cn.renlm.micro.common;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 import lombok.AllArgsConstructor;
 
 /**
@@ -12,6 +14,7 @@ import lombok.AllArgsConstructor;
 public enum RespCode {
 
 	// @formatter:off
+	UNKNOWN(0, "未知"), 
 	OK(200, "成功"), 
 	ERR(500, "失败"),
 	UNAUTHORIZED(401, "未登录"),
@@ -22,5 +25,33 @@ public enum RespCode {
 	public final int code;
 
 	public final String desc;
+
+	@JsonCreator
+	public static RespCode ofCode(int code) {
+		RespCode[] arr = RespCode.values();
+		for (int i = 0; i < arr.length; i++) {
+			RespCode rc = arr[i];
+			if (rc.code == code) {
+				return rc;
+			}
+		}
+		{
+			return UNKNOWN;
+		}
+	}
+
+	@JsonCreator
+	public static RespCode ofName(String name) {
+		RespCode[] arr = RespCode.values();
+		for (int i = 0; i < arr.length; i++) {
+			RespCode rc = arr[i];
+			if (rc.name().equals(name)) {
+				return rc;
+			}
+		}
+		{
+			return UNKNOWN;
+		}
+	}
 
 }
