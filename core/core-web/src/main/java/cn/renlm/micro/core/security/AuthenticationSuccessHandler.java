f3ab403ae529d4c7b8abfea26be240d24f1aa08d
renlm-a <renlm-a@glodon.com>
2025-01-05 16:44:12 +0800

    up

--- core/core-web/src/main/java/cn/renlm/micro/core/security/AuthenticationSuccessHandler.java
+++ core/core-web/src/main/java/cn/renlm/micro/core/security/AuthenticationSuccessHandler.java
@@ -2,10 +2,13 @@ package cn.renlm.micro.core.security;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.Resource;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -21,10 +24,18 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
+	private static final Logger log = LoggerFactory.getLogger(AuthenticationSuccessHandler.class);
+
+	@Resource
+	private UserDetailsService userDetailsService;
+
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			Authentication authentication) throws ServletException, IOException {
 		super.onAuthenticationSuccess(request, response, authentication);
+		userDetailsService.updateCurrentUser(request, response, principal -> {
+			log.debug("登录成功 - username: {}, hint: {}", principal.getUsername(), principal.getHint());
+		});
 	}
 
 }
