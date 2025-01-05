a26c5c4743ffc632005ec5e76fb220e5f14505b5
renlm-a <renlm-a@glodon.com>
2025-01-05 19:26:50 +0800

    up

--- core/core-web/src/main/java/cn/renlm/micro/core/CoreWebApplication.java
+++ core/core-web/src/main/java/cn/renlm/micro/core/CoreWebApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Lazy;
+
+import com.netflix.discovery.DiscoveryClient;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.Resource;
 
 /**
  * CoreWeb
@@ -16,8 +22,19 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 public class CoreWebApplication {
 
+	@Lazy
+	@Resource
+	private DiscoveryClient discoveryClient;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CoreWebApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+			discoveryClient.shutdown();
+		}));
 	}
 
 }
