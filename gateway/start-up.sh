427e3d784f287c3221ba80d4d570b0471b7642f3
renlm <renlm@21cn.com>
2025-01-07 01:31:11 +0800

    up

--- gateway/start-up.sh
+++ gateway/start-up.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-HOSTNAME=${POD_IP//./-}.${POD_SERVICE_NAME}.${POD_NAMESPACE}.svc.cluster.local
+HOSTNAME=${POD_IP//./-}.${RELEASE_NAME}.${POD_NAMESPACE}.svc.cluster.local
 export HOSTNAME=$HOSTNAME
 java -Djava.security.egd=file:/dev/./urandom -jar /app.jar
